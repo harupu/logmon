8ec9391e31201571d00535c6abf80cc6a7459768
Etsuji Nakai <enakai@redhat.com>
2016-02-04 13:57:25 +0900

    Initial commit for RHEL7/CentOS7

--- logmon.pl
+++ logmon.pl
@@ -77,9 +77,7 @@ my ( $tail_num, $target, $message, $action, $new_action, $line );
 
     ( $target, $tail_num ) = @_;
     unless ( fork() ) {      # Child
-        $SIG{HUP}  = \&catch_hup;
-        $SIG{TERM} = \&catch_term;
-        open ( IN, "tail -n$tail_num -F $target|" );
+        open ( IN, "tail -n $tail_num -F $target|" );
         while ( <IN> ) {
             foreach $message ( keys %{$Config{$target}} ) {
                 if ( $_ =~ m/$message/ ) {
--- setup.sh
+++ setup.sh
@@ -3,12 +3,9 @@
 mkdir -p /etc/logmon
 cp ./logmon.pl /etc/logmon/
 cp ./logmon.conf /etc/logmon/
-cp ./logmon /etc/init.d/logmon
+cp ./logmon.service /etc/systemd/system/
 
-chmod 700 /etc/init.d/logmon
 chmod 700 /etc/logmon/logmon.pl
 chmod 600 /etc/logmon/logmon.conf
 
-chkconfig --add logmon
-chkconfig --level 345 logmon on
-
+systemctl enable logmon.service
