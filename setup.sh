5f98f25351a02fe2da536c5d06ef3c56bb91a373
Etsuji Nakai <enakai@redhat.com>
2016-02-04 14:15:24 +0900

    Mod: allow exec from any directory.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -x
 
 mkdir -p /etc/logmon
+
+pushd $(dirname $0)
 cp ./logmon.pl /etc/logmon/
 cp ./logmon.conf /etc/logmon/
 cp ./logmon.service /etc/systemd/system/
+popd
 
 chmod 700 /etc/logmon/logmon.pl
 chmod 600 /etc/logmon/logmon.conf
 
+systemctl daemon-reload
 systemctl enable logmon.service
